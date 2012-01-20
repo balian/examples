ac751fba4af17327379fbde9edc44d19723b3755
unknown <developer@developer-PC.(none)>
2012-01-19 21:41:37 -0600

    Mensaje from delphi

---
+++ Unit2.dfm
@@ -0,0 +1,34 @@
+object Form2: TForm2
+  Left = 0
+  Top = 0
+  Caption = 'Form2'
+  ClientHeight = 150
+  ClientWidth = 253
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  PixelsPerInch = 96
+  TextHeight = 13
+  object Button1: TButton
+    Left = 8
+    Top = 8
+    Width = 75
+    Height = 25
+    Caption = 'Button1'
+    TabOrder = 0
+    OnClick = Button1Click
+  end
+  object memo: TMemo
+    Left = 89
+    Top = 8
+    Width = 161
+    Height = 131
+    Lines.Strings = (
+      'memo')
+    TabOrder = 1
+  end
+end
---
+++ Unit2.pas
@@ -0,0 +1,70 @@
+unit Unit2;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls, dbxjson,idhttp;
+
+type
+  TForm2 = class(TForm)
+    Button1: TButton;
+    memo: TMemo;
+    procedure Button1Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form2: TForm2;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm2.Button1Click(Sender: TObject);
+var
+  obj,jArray:TJsonArray;
+  obj2:TJsonobject;
+  str:string;
+  tam,i:Integer;
+  obj3:TJsonPair;
+begin
+  obj:=TJSONArray.Create;
+  obj.Add(1);
+  obj.Add(2);
+  obj.Add(3);
+  obj.Add(4);
+  obj.Add('foo');
+  obj.AddElement((TJSONArray.Create(TJsonNumber.Create(22))));
+  (obj.Get(5) as TJSONArray).Add('carlos adrian');
+
+  //showmessage(obj.ToString);
+
+  //('{"a":1}' as TJsonObject)
+  With tidhttp.create do begin
+   str:=get('http://localhost/jsontest.php');
+  end;
+  obj2 := TJSONObject.ParseJSONValue(TEncoding.ASCII.GetBytes(str), 0) as TJSONObject;
+  //showmessage(obj2.ToString);
+  tam:=obj2.Size;
+
+  jArray := (obj2.Get(0) as tjsonpair).JsonValue as TJsonArray; // pair value is an array
+  memo.Clear;
+  for i:=0 to jArray.Size-1 do
+   begin
+     obj3:= TJSONObject(jArray.Get(i)).Get(0);
+     str:=obj3.JsonValue.Value;
+
+     {memo.Lines.Add(str);
+     obj2 := TJSONObject.ParseJSONValue(TEncoding.ASCII.GetBytes(str), 0) as TJSONObject;
+     str:=obj2.Get(0).JsonValue.Value;}
+     memo.lines.Add(str);
+   end;
+
+
+end;
+
+end.
